cc572de1264fd12c903d73ea09ab6c418520a345
nidhigaikwad07-bit <nidhigaikwad07@gmail.com>
2026-04-01 21:25:33 +0530

    Add files via upload

---
+++ unit 4/Data_csv.py
@@ -0,0 +1,11 @@
+import csv
+
+row_count = 0
+
+with open ("book1" , "r") as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        print(row)
+        row_count += 1
+print("Total number of rows: , row_count")
